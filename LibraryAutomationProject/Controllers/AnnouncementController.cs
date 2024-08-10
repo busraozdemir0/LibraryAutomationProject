b4387530434f685007036ec25bd33f403df39757
Büşra <98536303+busraozdemir0@users.noreply.github.com>
2024-08-10 18:01:22 +0300

    Duyuruları anlık olarak AJAX yardımıyla listeleme - 30

---
+++ LibraryAutomationProject/Controllers/AnnouncementController.cs
@@ -0,0 +1,26 @@
+﻿using LibraryAutomation.Entities.DAL;
+using LibraryAutomation.Entities.Model.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LibraryAutomationProject.Controllers
+{
+    public class AnnouncementController : Controller
+    {
+        LibraryContext context = new LibraryContext();
+        AnnouncementDAL announcementDAL=new AnnouncementDAL();
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public JsonResult AnnouncementList()
+        {
+            var model = announcementDAL.GetAll(context);
+            return Json(model, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
