23d3e18d5cc43bef090df00c4417ab4356d06504
Büşra <98536303+busraozdemir0@users.noreply.github.com>
2024-08-12 16:38:14 +0300

    Rol ile ilgili CRUD işlemleri - 63

---
+++ LibraryAutomationProject/Controllers/RoleController.cs
@@ -0,0 +1,70 @@
+﻿using LibraryAutomation.Entities.DAL;
+using LibraryAutomation.Entities.Mapping;
+using LibraryAutomation.Entities.Model;
+using LibraryAutomation.Entities.Model.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LibraryAutomationProject.Controllers
+{
+    //[Authorize(Roles = "Admin")]
+    [AllowAnonymous]
+    public class RoleController : Controller
+    {
+        LibraryContext context = new LibraryContext();
+        RoleDAL roleDAL = new RoleDAL();
+        // GET: Role
+        public ActionResult Index()
+        {
+            var model = roleDAL.GetAll(context);
+            return View(model);
+        }
+
+        public ActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost, ValidateAntiForgeryToken]
+        public ActionResult Add(Role role)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(role);
+            }
+            roleDAL.InsertorUpdate(context, role);
+            roleDAL.Save(context);
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Update(int? id)
+        {
+            if (id == null)
+                return HttpNotFound("Id değeri girilmedi.");
+            var result = roleDAL.GetByFilter(context, x => x.Id == id);
+            return View(result);
+        }
+
+        [HttpPost, ValidateAntiForgeryToken]
+        public ActionResult Update(Role role)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(role);
+            }
+            roleDAL.InsertorUpdate(context, role);
+            roleDAL.Save(context);
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Delete(int? id)
+        {
+            roleDAL.Delete(context, x => x.Id == id);
+            roleDAL.Save(context);
+            return RedirectToAction("Index", "Role");
+        }
+    }
+}
