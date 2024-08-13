8708c612beb67aa9a15172761a07373571d155dd
Büşra <98536303+busraozdemir0@users.noreply.github.com>
2024-08-13 15:04:26 +0300

    Üye güncelleme/image işlemleri ve üye silme - 67

--- LibraryAutomationProject/Controllers/MemberController.cs
+++ LibraryAutomationProject/Controllers/MemberController.cs
@@ -37,21 +37,69 @@ namespace LibraryAutomationProject.Controllers
                 {
                     var image = Path.GetFileName(ImagePath.FileName);
                     string path = Path.Combine(Server.MapPath("~/images"), image); // resmin eklenecegi yol (images klasorune eklenecek)
-                    {
-                        if (System.IO.File.Exists(path) == false)
-                        {
-                            ImagePath.SaveAs(path);
-                        }
-                        member.ImagePath = "/images/" + image;
 
+                    if (System.IO.File.Exists(path) == false)
+                    {
+                        ImagePath.SaveAs(path);
                     }
-                    memberDAL.InsertorUpdate(context, member);
-                    memberDAL.Save(context);
-                    return RedirectToAction("Index");
+                    member.ImagePath = "/images/" + image;
                 }
+                memberDAL.InsertorUpdate(context, member);
+                memberDAL.Save(context);
+                return RedirectToAction("Index");
             }
 
             return View(member);
+        }
+
+        public ActionResult Update(int? id)
+        {
+            if (id == null)
+            {
+                return HttpNotFound();
+            }
+            var model = memberDAL.GetByFilter(context, x => x.Id == id);
+            return View(model);
+        }
+
+        [HttpPost, ValidateAntiForgeryToken]
+        public ActionResult Update(Member member, HttpPostedFileBase ImagePath) // HttpPostedFileBase => resim yuklemek icin (ImagePath dememizin sebebi view'daki gorsel kismindaki degisken adi ile ayni olmasi icin)
+        {
+            if (ModelState.IsValid)
+            {
+                var model = memberDAL.GetByFilter(context, x => x.Id == member.Id);
+
+                member.ImagePath = model.ImagePath; // Eger resim secilmemisse db'de var olan resim tekrar aktarilsin
+
+                // Sayet resim secilmisse Resim yukleme islemleri gerceklestiriliyor
+                if (ImagePath != null && ImagePath.ContentLength > 0) // ImagePath null degilse ve resim gercekten varsa
+                {
+                    var image = Path.GetFileName(ImagePath.FileName);
+                    string path = Path.Combine(Server.MapPath("~/images"), image); // resmin eklenecegi yol (images klasorune eklenecek)
+                    if (System.IO.File.Exists(path) == false)
+                    {
+                        ImagePath.SaveAs(path);
+                    }
+                    member.ImagePath = "/images/" + image;
+
+                }
+                memberDAL.InsertorUpdate(context, member);
+                memberDAL.Save(context);
+                return RedirectToAction("Index");
+            }
+
+            return View(member);
+        }
+
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return HttpNotFound();
+            }
+            memberDAL.Delete(context, x => x.Id == id);
+            memberDAL.Save(context);
+            return RedirectToAction("Index");
         }
     }
 }
